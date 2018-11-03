56b950ad93811c8fdfd9fd7e77aff0ae2c5c2fc3
Chrissy Zwainz <zchrissychrissy@gmail.com>
2018-11-03 09:00:47 -0600

    first commit

---
+++ food-game/app.js
@@ -0,0 +1,1 @@
+console.log("fruit game!")
--- slap-game/app.js
+++ slap-game/app.js
@@ -3,11 +3,10 @@ var health = 100;
 let snowman = {
   name: "Yeti",
   hits: 0,
-  statesOfHealth: ['Happy', 'Scared!', 'Under Avalanche'],
-  images: [],
-  items: []
+  statesOfHealth: ['Happy', 'Scared!', 'Under Avalanche', 'In Igloo'],
+  items: [],
+  images: ['', '', '', 'assets.igloo.jpg']
 }
-
 
 function snowball() {
   snowman.hits++
@@ -17,13 +16,13 @@ function snowball() {
 
 function icePick() {
   snowman.hits++
-  health -= 5
+  health -= 5 - addMods()
   update()
 }
 
 function avalanche() {
   snowman.hits++
-  health -= 10
+  health -= 10 - addMods()
   update()
 }
 
@@ -34,16 +33,25 @@ function reset() {
   update()
 }
 
-
 let items = {
   softCooler: { name: 'Soft Cooler', modifier: 2, description: "STOP" },
-  rambler: { name: 'Rambler', modifier: 4, description: "STOP" },
-  hardCooler: { name: 'Hard Cooler', modifier: 6, description: "I'm Going In the Cooler!" }
+  rambler: { name: 'Rambler', modifier: 5, description: "STOP" },
+  hardCooler: { name: 'Hard Cooler', modifier: 15, description: "I'm Going In the Cooler!" }
 }
 
 function addSoftCooler() {
   snowman.items.push(items.softCooler)
   console.log(snowman.items)
+}
+
+function addRambler() {
+  snowman.items.push(items.rambler)
+  snowman.items.push(items.softCooler)
+  console.log(snowman.items)
+}
+
+function addHardCooler() {
+  snowman.items.push(items.hardCooler)
 }
 
 function addMods() {
@@ -58,13 +66,15 @@ function addMods() {
 
 function update() {
   document.getElementById("health-level").innerText = "Health Level: " + health
-
+  document.getElementById('hits').innerText = "Hits: " + snowman.hits
   if (health > 75) {
     document.getElementById('health').innerText = snowman.statesOfHealth[0]
   } else if (health > 40) {
     document.getElementById('health').innerText = snowman.statesOfHealth[1]
   } else if (health < 25) {
     document.getElementById('health').innerText = snowman.statesOfHealth[2]
+    // need to change to igloo pic bc yeti goes into igloo
+    // } else if (health == 0 || snowman.hits == 20) {
+    //   document.getElementById('health').innerText = snowman.images[3]
   }
-  document.getElementById('hits').innerText = "Hits: " + snowman.hits
 }
